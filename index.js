700dfb389b0dadfcd0a0e420368a989b81791d98
Bhanu Dev Jaiswal <bhanuj@microsoft.com>
2015-09-10 14:20:34 -0700

    Updating index.js file

--- index.js
+++ index.js
@@ -1,57 +1,7 @@
 var fs = require('fs');
 var filePath = "log.txt";
 var reporter = function (runner) {
-  this.initialize.call(this, runner);
-};
-
-var console = {
-  log: function (message) {
-    fs.write(filePath, message);
-  }
-};
-
-
-function title(test) {
-  return test.fullTitle().replace(/#/g, '');
-}
-
-reporter.prototype.initialize = function (runner) {
-
-  var n = 1;
-  var passes = 0;
-  var failures = 0;
-  runner.on('start', function () {
-    console.log("Starting tests");
-    var total = runner.grepTotal(runner.suite);
-    console.log(1 + ".." + total);
-  });
-
-  runner.on('test end', function () {
-    ++n;
-  });
-
-  runner.on('pending', function (test) {
-    console.log('ok' + n + 'SKIP -' + title(test));
-  });
-
-  runner.on('pass', function (test) {
-    passes++;
-    console.log('ok' + n + title(test));
-  });
-
-  runner.on('fail', function (test, err) {
-    failures++;
-    console.log('not ok' + n + title(test));
-    if (err.stack) {
-      console.log(err.stack.replace(/^/gm, '  '));
-    }
-  });
-
-  runner.on('end', function () {
-    console.log('# tests ' + (passes + failures));
-    console.log('# pass ' + passes);
-    console.log('# fail ' + failures);
-  });
+  //this.initialize.call(this, runner);
 };
 
 module.exports = reporter;
